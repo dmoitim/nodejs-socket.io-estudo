4db41a8e0208255002943abd72d926d641bac8df
Devair Luiz Moitim <dmoitim@gmail.com>
2016-07-04 21:56:51 -0300

    Implementação do exemplo socket-chat

---
+++ socket-chat/README.md
@@ -0,0 +1,18 @@
+## socket-chat
+
+Esse projeto cria um chat simples que permite a visualização das mensagens entre vários usuários.
+Para executar o projeto siga os passos:
+
+1- Clone o repositório;
+
+2- Acesse o diretório do projeto;
+
+3- Execute o comando npm install para instalar as dependências (nesse caso o express e o socket.io);
+
+4- Execute o comando node index.js, ou nodemon index.js caso você já possua o pacote nodemon instalado;
+
+5- Acesso o endereço http://localhost:3000 em vários naveadores/abas e ao enviar uma mensagem em um deles você verá que ela será atualizada em todos os demais.
+
+##### Fonte:
+* [Socket.IO](http://socket.io/docs/) - Socket.IO enables real-time bidirectional event-based communication.
+* [Nomadev](http://nomadev.com.br/node-js-socket-io-manual-parte-1/) - O blog de um dev nômade
--- socket-chat/index.js
+++ socket-chat/index.js
@@ -1,10 +1,30 @@
+//Iniciando o servidor HTTP + Express + Socket.IO
 var app  = require('express')()
-   ,http = require('http').Server(app);
+   ,http = require('http').Server(app)
+   ,io   = require('socket.io')(http);
 
+//Função que devolverá a página para o usuário
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+//Evento connection é executado quando a página é chamada (usuário entra)
+io.on('connection', function(socket){
+    console.log('Um usuário se conectou');
+
+    //Atualiza para todos a mensagem enviada
+    socket.on('chat', function(msg){
+        console.log('mensagem: ' + msg)
+        io.emit('chat', msg);
+    });
+
+    //Evento disconnect é executado quando a página é fechada / reload (usuário sai)
+    socket.on('disconnect', function() {
+        console.log('Um usuário se desconectou');
+    });
+});
+
+//Definindo a porta que o servidor HTTP irá escutar
 http.listen(3000, function() {
     console.log('Servidor rodando na porta 3000');
 });
